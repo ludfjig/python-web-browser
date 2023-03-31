d61d4a72f58af736aca554b3e6e6c67885aee4d1
Ludfjig <ludfjig@hotmail.com>
2023-03-30 23:33:30 -0700

    pass test but hang

--- browser.py
+++ browser.py
@@ -1,1 +1,128 @@
-# The starter code is a blank file. Have fun :)
+import socket
+import ssl
+import sys
+import threading
+
+cache = {}
+
+
+def request(url, headers={}, depth=0):
+    if depth > 10:
+        raise Exception("Too many redirects")
+
+    # save original url
+    original_url = url
+
+    # format request
+    scheme, url = url.split("://", 1)
+    assert scheme in ["http", "https", "file"], f"Unknown scheme {scheme}"
+
+    # check cache
+    if scheme in ["http", "https"] and original_url in cache:
+        return cache[original_url]
+
+    if scheme == "file":
+        f = open(url)
+        body = f.read()
+        return {}, body
+
+    if "/" in url:
+        host, path = url.split("/", 1)
+        path = "/" + path
+    else:
+        host = url
+        path = "/"
+    port = 80 if scheme == "http" else 443
+
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+
+    # create socket
+    s = socket.socket()
+    if scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
+    s.connect((host, port))
+
+    request_headers = {"Host": host,
+                       "Connection": "close",
+                       "User-Agent": "TestBrowser"} | headers
+    request_headers = {k.lower(): v for k, v in request_headers.items()}
+
+    # construct request HTTP string
+    request_string = f"GET {path} HTTP/1.1\r\n"
+    for header, value in request_headers.items():
+        request_string += f"{header}: {value}\r\n"
+    request_string += "\r\n"
+
+    # send request
+    s.send(request_string.encode("utf8"))
+
+    # read response
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    status = int(status)
+    assert status == 200 or 300 <= status <= 399, "{}: {}".format(
+        status, explanation)
+
+    # parse headers into dict
+    response_headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n":
+            break
+        header, value = line.split(":", 1)
+        response_headers[header.lower()] = value.strip()
+
+    assert "transfer-encoding" not in response_headers
+    assert "content-encoding" not in response_headers
+
+    # follow redirects
+    if 300 <= status <= 399:
+        location = response_headers["location"]
+        if location.startswith("/"):
+            location = scheme + "://" + host + location
+        return request(location, depth=depth + 1)
+
+    # read body
+    body = response.read()
+
+    # cache response
+    if "cache-control" in response_headers and status == 200:
+        cache_control = response_headers["cache-control"]
+        if cache_control != "no-store":
+            assert cache_control.startswith(
+                "max-age="), "Unknown cache-control"
+            age = int(cache_control.split("=", 1)[1])
+            cache[original_url] = (response_headers, body)
+            threading.Timer(age, lambda: cache.pop(original_url)).start()
+
+    s.close()
+
+    return response_headers, body
+
+
+def show(body):
+    in_angle = False
+    for c in body:
+        if c == "<":
+            in_angle = True
+        elif c == ">":
+            in_angle = False
+        elif not in_angle:
+            print(c, end="")
+
+
+def load(url):
+    headers, body = request(url)
+    show(body)
+
+
+if __name__ == "__main__":
+    DEFAULT_FILE_URL = "file:///mnt/c/users/ludvig/uw/CSE493X/cse493x-23sp-ludvil/browser.py"
+    if len(sys.argv) == 1:
+        load(DEFAULT_FILE_URL)
+    else:
+        load(sys.argv[1])
